b46c07fd12e7004021f718af940dfc3543ccb381
Stephen Isaac <stephenisaac9292@gmail.com>
2025-05-27 15:09:04 +0100

    try2

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,5 +1,5 @@
--- INNER JOIN: Get all bookings and the respective users who made those bookings
-SELECT bookings.id AS booking_id, users.id AS user_id, users.name AS user_name, bookings.property_id, bookings.booking_date
-FROM bookings
-INNER JOIN users ON bookings.user_id = users.id
-ORDER BY bookings.id;
+-- LEFT JOIN: Get all properties and their reviews, including properties without reviews
+SELECT properties.id AS property_id, properties.name AS property_name, reviews.id AS review_id, reviews.comment
+FROM properties
+LEFT JOIN reviews ON properties.id = reviews.property_id
+ORDER BY properties.id;
